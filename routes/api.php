5d065def65cfc175f97eb9cb666b513c038693f3
Doston <dostonberuniy@gmail.com>
2022-02-10 15:51:22 +0500

    middleware

--- routes/api.php
+++ routes/api.php
@@ -14,12 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group([
-
-    'middleware' => 'api',
-    'prefix' => 'auth'
-
-], function ($router) {
+Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
 
     Route::post('login', [\App\Http\Controllers\AuthController::class, 'login'])->name('login');
     Route::post('logout', [\App\Http\Controllers\AuthController::class, 'logout']);
@@ -28,8 +23,9 @@ Route::group([
 
 });
 
-
-Route::group(['middleware' => 'auth'], function () {
-    Route::resource('companies', App\Http\Controllers\API\CompanyAPIController::class);
-    Route::resource('employees', App\Http\Controllers\API\EmployeeAPIController::class);
+Route::group(['middleware' => 'admin'], function () {
+   Route::resource('companies', App\Http\Controllers\API\CompanyAPIController::class);
+});
+Route::group(['middleware' => 'company'], function () {
+   Route::resource('employees', App\Http\Controllers\API\EmployeeAPIController::class);
 });
